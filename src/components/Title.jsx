3b432989ace77d97e2315373999df0a7189d326c
JorgeAlbertoLeonJimenez <130315621+JorgeAlbertoLeonJimenez@users.noreply.github.com>
2023-12-20 22:26:55 -0800

    Update Title.jsx

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import "./styles/title.css"
 export default function Title(props) {
+
+const { op , op2 } = props
+
   return (
     <div className='title-info'>
-          <p className='min'>{props.op}</p>
-          <p className='neg'><strong>{props.op2}</strong></p>
+          <p className='min'>{op}</p>
+          <p className='neg'><strong>{op2}</strong></p>
     </div>
   )
 }
